dbb22a4e3e43e8be64f481eee0f6206f15bd6e00
RemiLalonde2000 <remlalonde@hotmail.com>
2026-03-12 23:30:37 -0400

    a

--- ConvLSTM/submit.sh
+++ ConvLSTM/submit.sh
@@ -12,11 +12,7 @@
 echo "Job started on $(hostname) at $(date)"
 echo "Job ID: $SLURM_JOB_ID"
 
-module --force purge
-module load python/3.10
-module load cuda
-module load netcdf
-module load hdf5
+module load StdEnv/2020 gcc/9.3.0 cuda/11.4 python/3.10 netcdf hdf5/1.12.1
 
 PROJECT_DIR=/home/remilalonde/Project/IFT6759-Project
 source $PROJECT_DIR/.venv/bin/activate
