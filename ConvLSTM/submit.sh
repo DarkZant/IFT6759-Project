2ddb3c31d8c2e92133d3102cae4f4341f53a0a1c
RemiLalonde2000 <remlalonde@hotmail.com>
2026-03-13 01:35:39 -0400

    a

--- ConvLSTM/submit.sh
+++ ConvLSTM/submit.sh
@@ -5,16 +5,16 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
 #SBATCH --time=04:00:00
-#SBATCH --output=/home/remilalonde/Project/IFT6759-Project/logs/convlstm_%j.out
-#SBATCH --error=/home/remilalonde/Project/IFT6759-Project/logs/convlstm_%j.err
-#SBATCH --export=NONE
+#SBATCH --job-name=convlstm
+#SBATCH --output=/home/remilalonde/Project/IFT6759-Project/logs/%x_%j.out
+#SBATCH --error=/home/remilalonde/Project/IFT6759-Project/logs/%x_%j.err
 
 
 echo "Job started on $(hostname) at $(date)"
 echo "Job ID: $SLURM_JOB_ID"
 
 module --force purge
-export RSNT_ARCH=avx2
+
 module load StdEnv/2020 gcc/9.3.0 cuda/11.4 python/3.10 mpi4py/3.1.3
 
 PROJECT_DIR=/home/remilalonde/Project/IFT6759-Project
@@ -30,6 +30,7 @@ export HDF5_USE_FILE_LOCKING=FALSE
 export PYTHONUNBUFFERED=1
 export PYTHONNOUSERSITE=1
 export TRAIN_FOLDER=/project/def-sponsor00/shared_CN_B/climatenet_engineered/train
+export CHECKPOINT_DIR=/home/remilalonde/Project/IFT6759-Project/checkpoints/${SLURM_JOB_NAME}
 
 echo "Starting ConvLSTM training..."
 python ConvLSTM/train.py
