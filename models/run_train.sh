d2763c3bf5f0473b6081fc8bf2d9bc7ed9928a9b
medinammartin3 <medinammartin3@gmail.com>
2026-03-20 18:57:12 -0400

    Add run_train.sh

---
+++ models/run_train.sh
@@ -0,0 +1,31 @@
+  GNU nano 7.2                       run_train.sh
+#!/bin/bash
+#SBATCH --account=def-sponsor00
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=64G
+#SBATCH --time=04:00:00
+#SBATCH --output=/project/def-sponsor00/medinammartin/logs/climatenet_%j.out
+#SBATCH --export=NONE
+
+# Initialisation
+source /etc/profile
+
+# Chargement des modules
+module load python/3.10
+module load openmpi/4.1.1
+module load mpi4py/3.1.4
+
+# Création et activation de l'environnement
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+
+# Installation des paquets
+pip install --no-index netCDF4 torch torchvision scipy xarray h5netcdf h5py
+
+# Configuration pour fichiers HDF5
+export HDF5_USE_FILE_LOCKING=FALSE
+
+# Lancement de l'entraînement
+python -u train_attention_unet.py
+
