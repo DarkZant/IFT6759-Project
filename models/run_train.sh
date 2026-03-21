929f401d30b558c4b177a915eee7bf233ac81068
medinammartin3 <medinammartin3@gmail.com>
2026-03-21 17:32:21 -0400

    Fix file

--- models/run_train.sh
+++ models/run_train.sh
@@ -1,10 +1,9 @@
-  GNU nano 7.2                       run_train.sh
 #!/bin/bash
 #SBATCH --account=def-sponsor00
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=64G
-#SBATCH --time=04:00:00
+#SBATCH --time=06:00:00
 #SBATCH --output=/project/def-sponsor00/medinammartin/logs/climatenet_%j.out
 #SBATCH --export=NONE
 
@@ -21,7 +20,7 @@ virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
 
 # Installation des paquets
-pip install --no-index netCDF4 torch torchvision scipy xarray h5netcdf h5py
+pip install --no-index netCDF4 torch torchvision scipy xarray h5netcdf h5py matplotlib
 
 # Configuration pour fichiers HDF5
 export HDF5_USE_FILE_LOCKING=FALSE
